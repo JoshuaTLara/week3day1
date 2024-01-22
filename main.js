bb302a6b4ed71efcd76b570d2aa02b86ec0c9cde
Joshua Lara <joshualara0910@gmail.com>
2024-01-22 16:20:59 -0700

    here it is

---
+++ main.js
@@ -0,0 +1,57 @@
+import './style.css';
+import getRandomWord from './src/randomWord.js';
+import  setSharkImage  from './src/sharkImage.js';
+import { setupWord } from './src/word.js';
+import { isLetterInWord } from './src/word.js';
+import { revealLetterInWord} from './src/word.js';
+import setupGuesses from './src/guess.js';
+
+
+
+document.querySelector('#app').innerHTML = `
+  <section id="shark-img"></section>
+
+  <section id="game-status"></section>
+
+  <section id="word-container"></section>
+
+  <section id="letter-buttons"></section>
+`;
+
+const initSharkwords = () => {
+  let numWrong = 0;
+  const word = getRandomWord();
+  setSharkImage(document.querySelector('#shark-img'), numWrong)
+  // for debugging:
+  setupWord(document.querySelector('#word-container'), word);
+  console.log(`[INFO] Correct word is: ${word}`);
+
+  const handleGuess = (guessEvent, letter) => {
+    // Disable button after click
+    const button = guessEvent.target;
+    button.setAttribute('disabled', true);
+  
+    // Handle correct/incorrect guess
+    if (isLetterInWord(letter)) {
+      revealLetterInWord(letter);
+    } else {
+      numWrong += 1;
+      setSharkImage(document.querySelector('#shark-img'), numWrong);
+    }
+    const isWordComplete = Array.from(document.querySelectorAll('.letter-box')).every(
+      (el) => el.innerText !== '',
+    );
+    
+  };
+  setupGuesses(document.querySelector('#letter-buttons'), handleGuess);
+  
+const endGame = 
+
+};
+
+initSharkwords();
+
+
+
+
+
---
+++ src/randomWord.js
@@ -0,0 +1,24 @@
+
+import sample from 'lodash.sample';
+
+const words = [
+  'strawberry',
+  'orange',
+  'apple',
+  'banana',
+  'pineapple',
+  'kiwi',
+  'peach',
+  'pecan',
+  'eggplant',
+  'durian',
+  'peanut',
+  'chocolate',
+];
+
+function getRandomWord() {
+  return sample(words);
+}
+
+// (...excerpt)
+export default getRandomWord; // why a default export instead of a named export?
